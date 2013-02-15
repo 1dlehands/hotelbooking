63910aa8d39abbfa644552a0aebb77e9eaf767bd
Jeffrey Matthias <jeffrey@matthias.org>
2013-02-15 10:27:26 -0700

    Remove unneeded migration

--- db/migrate/20130215095442_change_booking_times_to_strings.rb
+++
@@ -1,9 +0,0 @@
-class ChangeBookingTimesToStrings < ActiveRecord::Migration
-  def up
-    change_column("bookings", "start_date", "string")
-    change_column("bookings", "end_date", "string")
-  end
-
-  def down
-  end
-end
