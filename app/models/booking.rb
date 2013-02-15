be7fe2dc029f3de96b97eea3b89375eb195694f9
Jeffrey Matthias <jeffrey@matthias.org>
2013-02-14 21:08:17 -0700

    Add validations to booking.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ActiveRecord::Base
   attr_accessible :end_date, :room_id, :start_date, :user_id
+
+  validates_presence_of :end_date, :room_id, :start_date, :user_id
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Booking do
+  before do
+    @booking = Booking.new(user_id: 1, room_id: 1, start_date: 10.days.from_now, end_date: 11.days.from_now)
+  end
 
+  subject { @booking }
+
+  it { should respond_to(:user_id) }
+  it { should respond_to(:room_id) }
+  it { should respond_to(:start_date) }
+  it { should respond_to(:end_date) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @booking.user_id = nil }
+    it { should_not be_valid}
+  end
+
+  describe "when room_id is not present" do
+    before { @booking.room_id = nil }
+    it { should_not be_valid}
+  end
+
+  describe "when start_date is not present" do
+    before { @booking.start_date = nil }
+    it { should_not be_valid}
+  end
+
+  describe "when end_date is not present" do
+    before { @booking.end_date = nil }
+    it { should_not be_valid}
+  end
 end
