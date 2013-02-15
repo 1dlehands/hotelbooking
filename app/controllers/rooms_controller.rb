34d0489051bed0a6b966cf3f0b9bb0a1446b624a
Jeffrey Matthias <jeffrey@matthias.org>
2013-02-14 21:49:36 -0700

    Make minor tweaks to routes and rooms_controller. Set up root.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,9 @@
 class RoomsController < ApplicationController
+
   def index
+    if !session[:user_id]
+      redirect_to '/signin'
+    end
+    #if params[:start_date] && params[:end_date]
   end
 end
